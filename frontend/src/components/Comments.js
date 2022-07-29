9cec6c602fddb1e01cc34a0dd1133a3ed324a33d
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2022-07-29 13:17:03 -0400

    changed likes button in comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -47,7 +47,7 @@ export default function Comments({ commentsArr, postId, likes }) {
                 <span className="date">{comment.date}</span>
                 <span className="text">{comment.text}</span>
                 <div className="commentLikesDiv">
-                    <span 
+                    {/* <span 
                         className={"arrow arrow-up " + (localStorage.getItem("post:" + postId + ":comment:" + index) === "up" && "selected")} 
                         onClick={() => updateComment(index, comment.likes, "up")}
                     />
@@ -55,7 +55,16 @@ export default function Comments({ commentsArr, postId, likes }) {
                     <span 
                         className={"arrow arrow-down " + (localStorage.getItem("post:" + postId + ":comment:" + index) === "down" && "selected")} 
                         onClick={() => updateComment(index, comment.likes, "down")}
-                    />
+                    /> */}
+                    <svg
+                        viewBox="0 0 32 29.6"
+                        className={"heart " + (localStorage.getItem("post:" + postId + ":comment:" + index) === "up" && "selected")}
+                        onClick={() => updateComment(index, comment.likes, "up")}
+                    >
+                        <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+                            c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+                    </svg> 
+                    <span className="likesCount">{comment.likes}</span>
                 </div>
             </div>
         )
